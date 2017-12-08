d47b8e87a391c5510767ad3bf415c22456bd6156
JRoldan686 <jroldan686@gmail.com>
2017-12-08 16:37:11 +0100

    Trabajo de Ficheros de Acceso a Datos

--- app/src/main/java/jrl/acdat/trabajoficheros/MainActivity.java
+++ app/src/main/java/jrl/acdat/trabajoficheros/MainActivity.java
@@ -1,11 +1,8 @@
 package jrl.acdat.trabajoficheros;
 
 import android.app.ProgressDialog;
-import android.graphics.Bitmap;
-import android.media.Image;
 import android.os.Bundle;
 import android.os.CountDownTimer;
-import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
@@ -15,7 +12,6 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.loopj.android.http.AsyncHttpClient;
 import com.loopj.android.http.FileAsyncHttpResponseHandler;
 import com.loopj.android.http.RequestParams;
 import com.loopj.android.http.TextHttpResponseHandler;
@@ -34,15 +30,15 @@ import cz.msebera.android.httpclient.Header;
 
 public class MainActivity extends AppCompatActivity {
 
-    //public static final String URLENLACES = "http://192.168.2.11/acceso/enlaces.txt";
-    //public static final String URLFRASES = "http://192.168.2.11/acceso/frases.txt";
-    //public static final String URLERRORES = "http://192.168.2.11/acceso/errors.php";
-    //public static final String URLENLACES = "http://192.168.1.5/curso1617/enlaces.txt";
-    //public static final String URLFRASES = "http://192.168.1.5/curso1617/frases.txt";
-    //public static final String URLERRORES = "http://192.168.1.5/curso1617/errors.php";
-    public static final String URLENLACES = "http://bitbits.hopto.org/ACDAT/enlaces.txt";
-    public static final String URLFRASES = "http://bitbits.hopto.org/ACDAT/frases.txt";
-    public static final String URLERRORES = "http://bitbits.hopto.org/ACDAT/errors.php";
+    public static final String URLIMAGENES = "http://alumno.mobi/~alumno/superior/roldan/imagenes.txt";
+    public static final String URLFRASES = "http://alumno.mobi/~alumno/superior/roldan/frases.txt";
+    public static final String URLERRORES = "http://alumno.mobi/~alumno/superior/roldan/errores.php";
+
+    public static final String FICHERORAW = "intervalo";
+    public static final String EXTENSIONFICHERORAW = ".txt";
+    public static final String FICHEROENLACES = "enlaces.txt";
+    public static final String FICHEROFRASES = "frases.txt";
+
     public static final String UTF8 = "utf-8";
     public static final int SEGUNDO = 1000;
     public static final int TIEMPO = 5 * SEGUNDO;
@@ -53,9 +49,13 @@ public class MainActivity extends AppCompatActivity {
     TextView txvDescarga;
     Memoria memoria;
     StringBuilder frases;
+    CountDownTimer cdtImagenes;
+    CountDownTimer cdtFrases;
     long intervalo = 1;
     int posicionImagen = 0;
     int posicionFrase = 0;
+    ArrayList<CountDownTimer> contadoresImagenes;
+    ArrayList<CountDownTimer> contadoresFrases;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -66,8 +66,10 @@ public class MainActivity extends AppCompatActivity {
         edtFrases = (EditText)findViewById(R.id.edtFrases);
         btnDescargar = (Button)findViewById(R.id.btnDescargar);
         btnDescargar.setOnClickListener(new View.OnClickListener() {
+
             @Override
             public void onClick(View view) {
+                pararAplicacion();      // Si esta la aplicacion corriendo, primero se paran los CountDownTimer en ejecucion
                 descargarImagenes();
                 descargarFrases();
             }
@@ -76,44 +78,48 @@ public class MainActivity extends AppCompatActivity {
         txvDescarga = (TextView)findViewById(R.id.txvDescarga);
         memoria = new Memoria(this);
         frases = new StringBuilder();
+        contadoresImagenes = new ArrayList<CountDownTimer>();
+        contadoresFrases = new ArrayList<CountDownTimer>();
 
-        edtImagenes.setText(URLENLACES);
+        edtImagenes.setText(URLIMAGENES);
         edtFrases.setText(URLFRASES);
 
         try {
-            intervalo = Long.valueOf(memoria.leerRaw("intervalo").getContenido());
+            intervalo = Long.valueOf(memoria.leerRaw(FICHERORAW).getContenido());
         } catch (Exception e) {
-            Toast.makeText(this, "No se ha podido leer el fichero intervalo.txt", Toast.LENGTH_LONG).show();
+            Toast.makeText(this, "No se ha podido leer el fichero \"" +
+                    FICHERORAW + EXTENSIONFICHERORAW + "\"", Toast.LENGTH_LONG).show();
         }
     }
 
     private void subirError(String mensajeError) {
-        final ProgressDialog progreso = new ProgressDialog(this);
-
         String error = new Date().toString() + " -> " + mensajeError;
         RequestParams params = new RequestParams();
         params.put("error", error);
         RestClient.post(URLERRORES, params, new TextHttpResponseHandler() {
-            @Override
-            public void onStart() {
-                super.onStart();
-                progreso.setProgressStyle(ProgressDialog.STYLE_SPINNER);
-                progreso.setMessage("Conectando . . .");
-                progreso.setCancelable(false);
-                progreso.show();
-            }
+
             @Override
             public void onFailure(int statusCode, Header[] headers, String responseString, Throwable throwable) {
-                progreso.dismiss();
-                Toast.makeText(MainActivity.this, "El error " + responseString +
-                        " no se ha subido al Servidor", Toast.LENGTH_LONG).show();
+                String mensaje;
+                if(responseString != null) {
+                    mensaje = responseString;
+                } else {
+                    mensaje = throwable.getLocalizedMessage();
+                }
+                Toast.makeText(MainActivity.this, "El error \"" + mensaje +
+                        "\" no se ha subido al Servidor", Toast.LENGTH_LONG).show();
             }
 
             @Override
             public void onSuccess(int statusCode, Header[] headers, String responseString) {
-                progreso.dismiss();
-                Toast.makeText(MainActivity.this, "El error " + responseString +
-                 " se ha subido con exito al Servidor", Toast.LENGTH_SHORT).show();
+                String mensaje;
+                if(responseString != null) {
+                    mensaje = responseString;
+                } else {
+                    mensaje = String.valueOf(statusCode);
+                }
+                Toast.makeText(MainActivity.this, "El error \"" + mensaje +
+                 "\" se ha subido con exito al Servidor", Toast.LENGTH_SHORT).show();
             }
         });
     }
@@ -154,22 +160,18 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void contadorImagenes(final String[] urlsImagenes, int tiempo, long interval) {
-
         final long milisegundos = intervalo;
-
-        new CountDownTimer(tiempo, interval) {
+        cdtImagenes = new CountDownTimer(tiempo, interval) {
 
             @Override
-            public void onTick(long l) {
-
-            }
+            public void onTick(long l) {}
 
             @Override
             public void onFinish() {
                 Picasso.with(MainActivity.this)
                         .load(urlsImagenes[posicionImagen++ % urlsImagenes.length])
-                        .placeholder(R.drawable.descargar)
-                        .error(R.drawable.error)
+                        .placeholder(R.drawable.descargar)      // Muestra una imagen con una flecha para indicar que se esta intentando descargar
+                        .error(R.drawable.error)                // Muestra una imagen con una X para indicar que no se ha podido descargar
                         .into(imgvDescarga, new Callback() {
 
                             @Override
@@ -179,9 +181,10 @@ public class MainActivity extends AppCompatActivity {
 
                             @Override
                             public void onError() {
-                                String mensaje = "La imagen de la ruta " +
-                                        urlsImagenes[posicionImagen % urlsImagenes.length] +
-                                        " no se ha descargado";
+                                int posicion = posicionImagen % urlsImagenes.length;
+                                String mensaje = "La imagen de la ruta \"" +
+                                        urlsImagenes[posicion] +
+                                        "\" no se ha descargado";
                                 Toast.makeText(MainActivity.this, mensaje, Toast.LENGTH_LONG).show();
                                 subirError(mensaje);
                                 contadorImagenes(urlsImagenes, TIEMPO, milisegundos);
@@ -189,18 +192,15 @@ public class MainActivity extends AppCompatActivity {
                         });
             }
         }.start();
+        contadoresImagenes.add(cdtImagenes);
     }
 
     private void contadorFrases(final String[] frases, int tiempo, long interval) {
-
         final long milisegundos = intervalo;
-
-        new CountDownTimer(tiempo, interval) {
+        cdtFrases = new CountDownTimer(tiempo, interval) {
 
             @Override
-            public void onTick(long l) {
-
-            }
+            public void onTick(long l) {}
 
             @Override
             public void onFinish() {
@@ -208,6 +208,7 @@ public class MainActivity extends AppCompatActivity {
                     contadorFrases(frases, TIEMPO, milisegundos);
             }
         }.start();
+        contadoresFrases.add(cdtFrases);
     }
 
     private void descargarImagenes() {
@@ -215,6 +216,7 @@ public class MainActivity extends AppCompatActivity {
         String url = String.valueOf(edtImagenes.getText());
         if(url != null) {
             RestClient.get(url, new FileAsyncHttpResponseHandler(this) {
+
                 @Override
                 public void onStart() {
                     super.onStart();
@@ -227,7 +229,7 @@ public class MainActivity extends AppCompatActivity {
                 @Override
                 public void onFailure(int statusCode, Header[] headers, Throwable throwable, File file) {
                     progreso.dismiss();
-                    String mensaje = "El fichero " + file.getPath() + " no se ha descargado";
+                    String mensaje = "El fichero \"" + FICHEROENLACES + "\" no se ha descargado";
                     Toast.makeText(MainActivity.this, mensaje, Toast.LENGTH_LONG).show();
                     subirError(mensaje);
                 }
@@ -235,9 +237,6 @@ public class MainActivity extends AppCompatActivity {
                 @Override
                 public void onSuccess(int statusCode, Header[] headers, File file) {
                     progreso.dismiss();
-                    //Toast.makeText(MainActivity.this, "El fichero " + file.getPath() +
-                    // " se ha descargado con exito", Toast.LENGTH_SHORT).show();
-
                     Resultado resultado = leer(file, UTF8);
                     String[] urlsImagenes = resultado.getContenido().split("\n");
                     contadorImagenes(urlsImagenes, 0, 0);
@@ -255,6 +254,7 @@ public class MainActivity extends AppCompatActivity {
         String url = String.valueOf(edtFrases.getText());
         if(url != null) {
             RestClient.get(url, new FileAsyncHttpResponseHandler(this) {
+
                 @Override
                 public void onStart() {
                     super.onStart();
@@ -267,7 +267,7 @@ public class MainActivity extends AppCompatActivity {
                 @Override
                 public void onFailure(int statusCode, Header[] headers, Throwable throwable, File file) {
                     progreso.dismiss();
-                    String mensaje = "El fichero " + file.getPath() + " no se ha descargado";
+                    String mensaje = "El fichero \"" + FICHEROFRASES + "\" no se ha descargado";
                     Toast.makeText(MainActivity.this, mensaje, Toast.LENGTH_LONG).show();
                     subirError(mensaje);
                 }
@@ -275,9 +275,6 @@ public class MainActivity extends AppCompatActivity {
                 @Override
                 public void onSuccess(int statusCode, Header[] headers, File file) {
                     progreso.dismiss();
-                    //Toast.makeText(MainActivity.this, "El fichero " + file.getPath() +
-                    // " se ha descargado con exito", Toast.LENGTH_SHORT).show();
-
                     Resultado resultado = leer(file, UTF8);
                     String[] frases = resultado.getContenido().split("\n");
                     contadorFrases(frases, 0, 0);
@@ -288,5 +285,32 @@ public class MainActivity extends AppCompatActivity {
             Toast.makeText(this, mensaje, Toast.LENGTH_LONG).show();
             subirError(mensaje);
         }
+    }
+
+    private void pararAplicacion() {
+        // Se paran todos los CountDownTimer en ejecución desde el último hasta el primero
+        for(int i = contadoresImagenes.size() - 1; i > 0; i--) {
+            CountDownTimer contador = contadoresImagenes.get(i);
+            if (contador != null) contador.cancel();
+        }
+        for(int i = contadoresFrases.size() - 1; i > 0; i--) {
+            CountDownTimer contador = contadoresFrases.get(i);
+            if (contador != null) contador.cancel();
+        }
+        // Se inicializan las posiciones
+        posicionImagen = 0;
+        posicionFrase = 0;
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        pararAplicacion();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        pararAplicacion();
     }
 }
